2b0aff1b126451cce0374c1da327cc96893f7abe
Ole Jørgen Knoph <s354371@oslomet.no>
2021-08-23 11:45:09 +0200

    Lagt til filer

--- Uke1/KildeKode/src/HelloWorld.java
+++ Uke1/KildeKode/src/HelloWorld.java
@@ -1,8 +1,12 @@
 public class HelloWorld {
 
     public static void main(String[] args){
-        for (int  i = 1; i <= 10; ++i){
-            System.out.print(i +  " ");
+        System.out.println("Hello World");
+        System.out.println();
+
+        System.out.println("Telle til ti");
+        for (int i = 1; i <= 10; ++i){
+            System.out.print(i + " ");
         }
     }
 }
