dbfe2fcb89add10f9b593e6743905152586648ae
Ole Jørgen Knoph <s354371@oslomet.no>
2021-08-31 11:28:06 +0200

    Lagt til testing og ordnet kildekode

--- Uke1/KildeKode/HybridForelesning24aug/src/AlgDat24aug.java
+++ Uke1/KildeKode/HybridForelesning24aug/src/AlgDat24aug.java
@@ -1,47 +1,64 @@
+
+/**
+ * Windows: [alt] + [enter] mens du holder på klassen for å lage tester
+ * Mac: [option] + [enter] --"--
+ */
 public class AlgDat24aug {
+
     public static void main(String[] args) {
-        System.out.println("Hybrid forelesning 24aug");
-        System.out.println();
+        System.out.println("Hello algdat");
 
-        int[] values = {1, 7, 2, 4, 6, 9};
+        int values[] = {1, 7, 2, 4, 6, 9};
 
+        myFirstSort(values);
+    }
 
-        for (int k = 0; k < values.length - 1; ++k) {
-
-            //Sjekk at vi får fornuftig svar for tilfellet vårt.
-            //Vi må utføre ordentlig testing før
-            int max_index = findMax(values, 0, values.length);
-            System.out.println("Største verdi ligger på plass " + max_index + " og har verdi " + values[max_index]);
+    /**
+     * Denne funksjonen tar inn ett array med verdier (heltall),
+     * og sorterer dem "in place"
+     * @param values Verdier vi skal sortere.
+     */
+    public static void myFirstSort(int[] values) {
+        for (int k=0; k<values.length-1; ++k) {
+            //Sjekk at vi får fornuftig svar for *ett* tilfelle.
+            //Vi må utføre ordentlig testing før vi
+            //faktisk kan stole på kildekoden vår
+            int max_index = findMax(values, k, values.length);
+            //System.out.println("Største verdi ligger på plass "
+            //        + max_index + " og har verdi " + values[max_index]);
 
             //Bytte plass på tall på plass 0 og max_index
             int temp = values[k];
             values[k] = values[max_index];
             values[max_index] = temp;
 
-            System.out.println();
-            System.out.println("Arrayet etter ombytting");
-            for (int i = 0; i < values.length; ++i) {
-                System.out.print(values[i] + " ");
-            }
-            System.out.println();
+            //System.out.println("Bytter plass " + k + " med plass " + max_index);
+            //System.out.println("Arrayet etter ombytting");
+            //for (int i = 0; i < values.length; ++i) {
+            //    System.out.print(values[i] + ", ");
+            //}
+            //System.out.println();
         }
     }
 
-    /*
-    FindMax - finner index til største tall i et array
-    men søker bare innenfor tallene i fra index [fra] til [til]
+    /**
+     * Findmax - finner index til største tall i et array,
+     * men søker bare innenfor tallene i [fra, til)
      */
-    public static int findMax (int[] values, int fra, int til){
-
-        //initialisert variablene
+    public static int findMax(int[] values, int fra, int til) {
+        //Initialiser ved å se på første "kort"
+        //Dette er det største tallet jeg har funnet
+        //så langt
         int index = fra;
         int max_value = values[fra];
 
-        for (int i = fra+1; i < til; ++i){
-
-            if (values[i] > max_value){
-                index = i;
+        //Sjekk at grensene for løkken er riktig.
+        // [1, values.length)
+        for (int i=fra+1; i<til; ++i) {
+            //Sjekk om vi har funnet nytt største tall
+            if (values[i] > max_value) {
                 max_value = values[i];
+                index = i;
             }
         }
 
